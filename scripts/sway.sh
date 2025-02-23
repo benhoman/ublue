761edfcf9f30cc439d820ba6c02d35e80c45f96b
Ben Homan <ben@benhoman.com>
2025-02-23 00:31:56 -0500

    updates

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -5,19 +5,21 @@ set ${SET_X:+-x} -eou pipefail
 echo "Installing Sway"
 
 dnf5 install -y \
-	sway \
-	swaylock \
-	swayidle \
-	rofi-wayland \
-	bemenu \
-	j4-dmenu-desktop \
-	wofi \
-	slurp \
-	grim \
-	wlr-randr \
-	wlsunset \
-	brightnessctl \
-	kanshi \
-	dunst \
-	mako
-
+  sway \
+  swaylock \
+  swayidle \
+  rofi-wayland \
+  xdg-desktop-portal \
+  wl-clipboard \
+  waybar \
+  bemenu \
+  j4-dmenu-desktop \
+  wofi \
+  slurp \
+  grim \
+  wlr-randr \
+  wlsunset \
+  brightnessctl \
+  kanshi \
+  dunst \
+  mako
