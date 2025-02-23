58ed9e8e149067ed98de49fde2526ae10c40f481
Ben Homan <ben@benhoman.com>
2025-02-22 23:54:18 -0500

    fix file

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -8,20 +8,16 @@ dnf5 install -y \
 	sway \
 	swaylock \
 	swayidle \
-	# launcher \
 	rofi-wayland \
 	bemenu \
 	j4-dmenu-desktop \
 	wofi \
-	# screenshot \
 	slurp \
 	grim \
-	# display \
 	wlr-randr \
 	wlsunset \
 	brightnessctl \
 	kanshi \
-	# notifications \
 	dunst \
 	mako
 
