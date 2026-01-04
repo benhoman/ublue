3e9e5c9b252cc7601def5918f059dcf5fad04049
Ben Homan <ben@benhoman.com>
2026-01-03 23:37:20 -0500

    update installer scripts now that opt is immutable

--- build_files/scripts/install-twingate.sh
+++ build_files/scripts/install-twingate.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env sh
 
-# Thanks to bri for the inspiration! My script is mostly based on this example:
-# https://github.com/briorg/bluefin/blob/c62c30a04d42fd959ea770722c6b51216b4ec45b/scripts/1password.sh
-
 set ${SET_X:+-x} -eou pipefail
 
 echo "Installing Twingate"
-
-# On libostree systems, /opt is a symlink to /var/opt,
-# which actually only exists on the live system. /var is
-# a separate mutable, stateful FS that's overlaid onto
-# the ostree rootfs. Therefore we need to install it into
-# /usr/lib/1Password instead, and dynamically create a
-# symbolic link /opt/1Password => /usr/lib/1Password upon
-# boot.
-
-# Prepare staging directory
-mkdir -p /var/opt # -p just in case it exists
-# for some reason...
 
 # Setup repo
 cat <<EOF >/etc/yum.repos.d/packages.twingate.com_rpm_.repo
